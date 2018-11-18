a74f600a1eb6301f2f17de4dc5cdf2867928d955
Adrov Igor <nucleartux@gmail.com>
2018-11-18 15:09:17 +0300

    add not ok case

--- client/src/FooComp.tsx
+++ client/src/FooComp.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import BarComp from '@shared/BarComp'
+import StyledComponent from "@shared/StyledComponent";
 
 // ok
 export default class FooComp extends React.Component {
     render(): React.ReactNode {
         return <div>
             <BarComp paramb={} parama={}/>
+            <StyledComponent /*foo=* - not ok*/>ddsds</StyledComponent>
         </div>
     }
 }
---
+++ shared/src/StyledComponent.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const StyledComponent = styled.div<{foo: string}>`
+    font-weight: bold;
+`
+
+export default StyledComponent
